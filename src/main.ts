f98cb628dc02ea7d6124cf12103f20e8a37395ce
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-06-13 18:32:14 -0400

    ✨ Use global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 import * as morgan from 'morgan';
 
 import { AppModule } from './app.module';
-import { CORS } from './constants';
+import { API_PREFIX, CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(morgan('dev'));
+  app.enableCors(CORS);
+  app.setGlobalPrefix(API_PREFIX);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
 
   const configService = app.get(ConfigService);
-
-  app.enableCors(CORS);
-
-  app.setGlobalPrefix('api/v1');
-
   await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
