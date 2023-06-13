fccf0fa11e4633abbf46bd3233b3aad1616cc0b4
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-06-12 23:31:03 -0400

    🔖 Set global prefix

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
 
+  app.setGlobalPrefix('api/v1');
+
   await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
