ba49cb59551e03aaa74ce1ee8a2564d9dada93ac
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-06-13 17:50:31 -0400

    🔧 Config typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { DataSourceConfig } from './config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({ ...DataSourceConfig }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/index.ts
@@ -0,0 +1,1 @@
+export { AppDataSource, DataSourceConfig } from './data-source.config';
