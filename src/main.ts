45f1402dc29944ded775ed2572af22b6469857a7
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-06-12 23:26:54 -0400

    ✨ Use morgan

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('dev'));
 
   const configService = app.get(ConfigService);
 
