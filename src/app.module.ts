69bd0756fd75ce1e55ac4e13c830b3499ca551fd
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-06-17 03:25:34 -0400

    🚧 Create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { DataSourceConfig } from './core/config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({ ...DataSourceConfig }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
