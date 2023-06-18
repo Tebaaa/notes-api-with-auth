b0d11bde017541aa41737c69fa376ea6eff13a3d
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-06-18 00:10:13 -0400

    ✨ Add login endpoint

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+import { serializeSingleResponse } from '@Core/utils';
+import { SingleResponseDoc } from '@Core/docs';
+import { ApiSingleResponse } from '@Core/decorators';
+import { UserDoc } from '@Users/docs';
+
+import { AuthService } from '../services';
+import { LoginDto } from '../dto';
+
+@ApiTags('Authentication endpoints')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @ApiSingleResponse(UserDoc)
+  @ApiOperation({
+    description: 'User this endpoint to authenticate',
+    summary: 'Login',
+  })
+  @Post('login')
+  async login(@Body() loginDto: LoginDto): Promise<SingleResponseDoc<UserDoc>> {
+    const user = await this.authService.login(loginDto);
+    console.log(user);
+
+    return serializeSingleResponse(UserDoc, user);
+  }
+}
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { isEmail } from 'class-validator';
+import { compare } from 'bcrypt';
+
+import { User } from '@Users/entities';
+
+import { LoginDto } from '../dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly eventEmitter: EventEmitter2) {}
+
+  async login({ user, password }: LoginDto): Promise<User> {
+    let loggedInUser: unknown;
+    if (isEmail(user)) {
+      [loggedInUser] = await this.eventEmitter.emitAsync(
+        'getOneUserByEmail',
+        user,
+      );
+    } else {
+      [loggedInUser] = await this.eventEmitter.emitAsync(
+        'getOneUserByUsername',
+        user,
+      );
+    }
+    if (!(loggedInUser instanceof User)) {
+      throw new UnauthorizedException('Username or email is incorrect');
+    }
+    const correctPassword = await compare(password, loggedInUser.password);
+    if (!correctPassword) {
+      throw new UnauthorizedException('Password is incorrect');
+    }
+    return loggedInUser;
+  }
+}
