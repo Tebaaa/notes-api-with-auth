b55702b2e60592676a8361542c050443cfc26876
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-06-18 23:04:50 -0400

    ✨ Add providers & imports to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { JwtModuleConfig } from '@Core/config';
 
 import { AuthController } from './controllers';
-import { AuthService } from './services';
+import { AuthService, TokenService } from './services';
+import { Token } from './entities';
+import { TokenRepository } from './repositories';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync(JwtModuleConfig),
+    TypeOrmModule.forFeature([Token]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, TokenService, TokenRepository],
 })
 export class AuthModule {}
