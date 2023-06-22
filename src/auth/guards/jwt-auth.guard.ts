9cca8d2237bdcc741ce0fe66da7c4ccc25b10e73
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-06-21 22:29:58 -0400

    🐛 Separate guards so user is saved in request headers

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,36 +1,5 @@
-import {
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
-import { EventEmitter2 } from '@nestjs/event-emitter';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private readonly eventEmitter: EventEmitter2) {
-    super();
-  }
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const req = context.switchToHttp().getRequest();
-    const [accessToken] = await this.eventEmitter.emitAsync(
-      'getTokenfromBearer',
-      req,
-    );
-    const [tokenExistsInDB] = await this.eventEmitter.emitAsync(
-      'getDBTokenByAccessToken',
-      accessToken,
-    );
-
-    // TODO: revisar en la blacklist :)
-    const [isBlacklisted] = await this.eventEmitter.emitAsync(
-      'getBlacklistedTokenByAccessToken',
-      accessToken,
-    );
-    if (!tokenExistsInDB || isBlacklisted) {
-      throw new UnauthorizedException('Invalid token');
-    }
-    return true;
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
