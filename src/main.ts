6f8d3225a51ec47283f4f166708a463a37a74297
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-06-28 01:37:35 -0400

    🦺 Add forbidNoWhiteList = true to validation pipe opts

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
     new ValidationPipe({
       transform: true,
       whitelist: true,
+      forbidNonWhitelisted: true,
       transformOptions: { enableImplicitConversion: true },
     }),
   );
