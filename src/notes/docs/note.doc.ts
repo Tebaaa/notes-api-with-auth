3537b424ffd69f0c3a3d30b4b51acd1bed5ef5c1
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-07-01 01:31:03 -0400

    📝 Create note doc

---
+++ src/notes/docs/note.doc.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+
+export class NoteDoc {
+  @ApiProperty()
+  @Expose()
+  id: string;
+
+  @ApiProperty()
+  @Expose()
+  createdAt: Date;
+
+  @ApiProperty()
+  @Expose()
+  updatedAt: Date;
+
+  @ApiProperty()
+  @Expose()
+  title: string;
+
+  @ApiProperty()
+  @Expose()
+  content: string;
+}
