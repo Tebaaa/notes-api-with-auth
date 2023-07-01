f3288d9ef7c4f605f6e1f7686660db537f73646c
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-07-01 01:38:27 -0400

    🦺 Add fields to CreateNoteDto

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,1 +1,16 @@
-export class CreateNoteDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength } from 'class-validator';
+
+import { Note } from '../entities';
+
+export class CreateNoteDto implements Partial<Note> {
+  @ApiProperty()
+  @IsString()
+  @MinLength(2)
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  @MinLength(2)
+  content: string;
+}
--- src/notes/dto/update-note.dto.ts
+++ src/notes/dto/update-note.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateNoteDto } from './create-note.dto';
+
+import { CreateNoteDto } from '.';
 
 export class UpdateNoteDto extends PartialType(CreateNoteDto) {}
