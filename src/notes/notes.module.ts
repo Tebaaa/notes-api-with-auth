b6a9a359ea1d75a911a9a907a8696b31e11cc922
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-07-01 01:39:19 -0400

    ✨ Add missing imports in note's module

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { NotesService } from './services';
 import { NotesController } from './controllers';
+import { NotesRepository } from './repositories';
+import { Note } from './entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Note])],
   controllers: [NotesController],
-  providers: [NotesService],
+  providers: [NotesService, NotesRepository],
 })
 export class NotesModule {}
