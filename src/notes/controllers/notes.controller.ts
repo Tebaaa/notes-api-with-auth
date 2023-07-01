2d9ae82fff2097b2848c854dea21d707fb64f0fc
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-07-01 01:40:33 -0400

    ✨ Create note's CRUD endpoints

--- src/notes/controllers/notes.controller.ts
+++ src/notes/controllers/notes.controller.ts
@@ -6,37 +6,121 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Query,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+
+import { AccessTokenGuard, JwtAuthGuard } from '@Auth/guards';
+import {
+  ApiPaginatedResponse,
+  ApiSingleResponse,
+  ReqUser,
+} from '@Core/decorators';
+import {
+  serializeMultipleResponse,
+  serializeSingleResponse,
+} from '@Core/utils';
+import { ICurrentUser } from '@Core/interfaces';
+import { PaginationDto } from '@Core/dtos';
+import { MultipleResponseDoc, SingleResponseDoc } from '@Core/docs';
 
 import { NotesService } from '../services';
-import { CreateNoteDto, UpdateNoteDto } from '../dto';
+import { CreateNoteDto, QueryArchivedDto, UpdateNoteDto } from '../dto';
+import { NoteDoc } from '../docs';
 
+@ApiBearerAuth()
+@ApiTags('Notes management endpoints')
+@UseGuards(JwtAuthGuard, AccessTokenGuard)
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
 
+  @ApiSingleResponse(NoteDoc)
+  @ApiOperation({
+    description: 'Use this endpoint to create a new note',
+    summary: 'Create a note',
+  })
   @Post()
-  create(@Body() createNoteDto: CreateNoteDto) {
-    return this.notesService.create(createNoteDto);
+  async create(
+    @Body() createNoteDto: CreateNoteDto,
+    @ReqUser() user: ICurrentUser,
+  ) {
+    return this.notesService.create(createNoteDto, user);
   }
 
+  @ApiPaginatedResponse(NoteDoc)
+  @ApiOperation({
+    description: `
+Use this endpoint to retrieve your active or inactive notes.\n
+Use 'archived' query parameter to choose the state of the notes.\n
+Use 'archived=true' to retrieve archived notes.\n
+Use 'archived=false' to retrieve active notes.\n
+If no value is given, active notes will be retrieved.
+`,
+    summary: 'Retrieves active/inactive notes',
+  })
   @Get()
-  findAll() {
-    return this.notesService.findAll();
+  async findAll(
+    @ReqUser() user: ICurrentUser,
+    @Query() archived: QueryArchivedDto,
+    @Query() pagination: PaginationDto,
+  ): Promise<MultipleResponseDoc<NoteDoc[]>> {
+    const [notes, total] = await this.notesService.findAll(
+      user,
+      archived.archived,
+      pagination,
+    );
+    return serializeMultipleResponse(NoteDoc, notes, pagination, total);
   }
 
+  @ApiSingleResponse(NoteDoc)
+  @ApiOperation({
+    description: 'Use this endpoint to find a note from a given id',
+    summary: 'Find a note',
+  })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.notesService.findOne(+id);
+  async findOne(
+    @Param('id') id: string,
+    @ReqUser() user: ICurrentUser,
+  ): Promise<SingleResponseDoc<NoteDoc>> {
+    const note = await this.notesService.findOneById(id, user);
+    return serializeSingleResponse(NoteDoc, note);
   }
 
+  @ApiSingleResponse(NoteDoc)
+  @ApiOperation({
+    description: `Use this endpoint to update a note's title and/or description`,
+    summary: 'Update a note',
+  })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
-    return this.notesService.update(+id, updateNoteDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateNoteDto: UpdateNoteDto,
+    @ReqUser() user: ICurrentUser,
+  ): Promise<SingleResponseDoc<NoteDoc>> {
+    const note = await this.notesService.update(id, updateNoteDto, user);
+    return serializeSingleResponse(NoteDoc, note);
   }
 
+  @ApiResponse({ status: HttpStatus.NO_CONTENT })
+  @ApiOperation({
+    description: `Use this endpoint to delete a note given its id`,
+    summary: `Delete a note`,
+  })
+  @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.notesService.remove(+id);
+  remove(
+    @Param('id') id: string,
+    @ReqUser() user: ICurrentUser,
+  ): Promise<void> {
+    return this.notesService.remove(id, user);
   }
 }
--- src/notes/services/notes.service.ts
+++ src/notes/services/notes.service.ts
@@ -1,26 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { ICurrentUser } from '@Core/interfaces';
+import { PaginationDto } from '@Core/dtos';
 
 import { CreateNoteDto, UpdateNoteDto } from '../dto';
+import { NotesRepository } from '../repositories';
+import { Note } from '../entities';
 
 @Injectable()
 export class NotesService {
-  create(createNoteDto: CreateNoteDto) {
-    return 'This action adds a new note';
+  constructor(private readonly notesRepository: NotesRepository) {}
+  async create(
+    createNoteDto: CreateNoteDto,
+    currentUser: ICurrentUser,
+  ): Promise<Note> {
+    const titleInUse = await this.notesRepository.findOneNoteByTitle(
+      createNoteDto.title,
+      currentUser,
+    );
+
+    const noteBelongsToUser = titleInUse?.user.id === currentUser.sub;
+    if (titleInUse && noteBelongsToUser) {
+      throw new ConflictException(
+        `Note with title '${titleInUse.title}' already exists`,
+      );
+    }
+
+    const noteToCreate = {
+      ...createNoteDto,
+      user: {
+        id: currentUser.sub,
+      },
+    };
+    const noteToSave = this.notesRepository.create(noteToCreate);
+    return this.notesRepository.save(noteToSave);
   }
 
-  findAll() {
-    return `This action returns all notes`;
+  findAll(
+    user: ICurrentUser,
+    isArchived: boolean,
+    pagination: PaginationDto,
+  ): Promise<[Note[], number]> {
+    return this.notesRepository.findAll(user, isArchived, pagination);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} note`;
+  async findOneById(id: string, currentUser: ICurrentUser): Promise<Note> {
+    const note = await this.notesRepository.findOneNoteById(id, currentUser);
+    if (!note) {
+      throw new NotFoundException(`Note with id ${id} not found`);
+    }
+    const noteBelongsToDiffUser = note.user.id !== currentUser.sub;
+    if (noteBelongsToDiffUser) {
+      throw new ConflictException(
+        `Note with id ${id} belongs to different user`,
+      );
+    }
+    return note;
   }
 
-  update(id: number, updateNoteDto: UpdateNoteDto) {
-    return `This action updates a #${id} note`;
+  async update(
+    id: string,
+    updateNoteDto: UpdateNoteDto,
+    currentUser: ICurrentUser,
+  ): Promise<Note> {
+    const note = await this.findOneById(id, currentUser);
+
+    if (updateNoteDto.title) {
+      const newTitleInUse = await this.notesRepository.findOneNoteByTitle(
+        note.title,
+        currentUser,
+      );
+      if (newTitleInUse) {
+        throw new ConflictException(
+          `Note with title ${updateNoteDto.title} already exists`,
+        );
+      }
+      note.title = updateNoteDto.title;
+    }
+
+    if (updateNoteDto.content) {
+      note.content = updateNoteDto.content;
+    }
+    return this.notesRepository.save({ id });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} note`;
+  async remove(id: string, currentUser: ICurrentUser): Promise<void> {
+    const note = await this.findOneById(id, currentUser);
+    this.notesRepository.remove(note);
+    return;
   }
 }
